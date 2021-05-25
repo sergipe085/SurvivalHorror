6524feedd13e95201cc02cf834b498858a253ad2
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-24 22:25:09 -0300

    animator state machine study

---
+++ Assets/Game/Scripts/SmoothCameraMount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraMount : MonoBehaviour
+{
+    public Transform Mount  =  null;
+    public float     Speed  =  5.0f;
+
+    private void LateUpdate() {
+        transform.position = Vector3.Lerp(transform.position, Mount.position, Time.deltaTime * Speed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Mount.rotation, Time.deltaTime * Speed);
+    }
+}
